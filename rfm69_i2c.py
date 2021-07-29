354e7b701027ad0c63961e6fd9f0125bdb2999dd
Tom Hoglund <infrapale@gmail.com>
2021-07-29 20:05:38 +0300

    First rfm69 commands implemented

--- rfm69_i2c.py
+++ rfm69_i2c.py
@@ -27,7 +27,8 @@ __repo__ = "https://github.com/infrapale/VillaAstrid_Portal"
 
 # Internal constants:
 _RFM69_I2C_ADDRESS    = const(0x20)
-_I2C_BUF_LEN          = const(16)
+_I2C_BUF_LEN          = const(32+2)
+_RFM69_BUF_LEN        = const(_I2C_BUF_LEN *2)
 _RFM69_RESET          = const(0x01)
 _RFM69_CLR_RX         = const(0x02)
 _RFM69_CLR_TX         = const(0x03)
@@ -39,3 +40,82 @@ _RFM69_RX_RD_MSG1     = const(0x42)
 _RFM69_RX_RD_MSG2     = const(0x43)
 _RFM69_RX_RD_LEN      = const(0x44)
 _RFM69_TX_FREE        = const(0x50)
+
+# User-facing constants:
+
+class RFM69_I2C:
+    # Class-level buffer to reduce memory usage and allocations.
+    # Note this is NOT thread-safe or re-entrant by design.
+    _BUFFER     = bytearray(_I2C_BUF_LEN)
+    _RFM69_BUFF = bytearray(_RFM69_BUF_LEN )
+
+    def __init__(self, i2c, address=_RFM69_I2C_ADDRESS):
+        self._device = i2c_device.I2CDevice(i2c, address)
+        # Verify the chip ID.
+        """if self._read_u8(_TSL2591_REGISTER_DEVICE_ID) != 0x50:
+            raise RuntimeError("Failed to find TSL2591, check wiring!")
+        """
+        # Put the device in a powered on state after initialization.
+        self.enable()
+        
+    def _read_u8(self, address):
+        # Read an 8-bit unsigned value from the specified 8-bit address.
+        with self._device as i2c:
+            # Make sure to add command bit to read request.
+            self._BUFFER[0] = (address) & 0xFF
+            i2c.write_then_readinto(self._BUFFER, self._BUFFER, out_end=1, in_end=1)
+        return self._BUFFER[0]
+
+    # Disable invalid name check since pylint isn't smart enough to know LE
+    # is an abbreviation for little-endian.
+    # pylint: disable=invalid-name
+    def _read_u16LE(self, address):
+        # Read a 16-bit little-endian unsigned value from the specified 8-bit
+        # address.
+        with self._device as i2c:
+            # Make sure to add command bit to read request.
+            self._BUFFER[0] = ( address) & 0xFF
+            i2c.write_then_readinto(self._BUFFER, self._BUFFER, out_end=1, in_end=2)
+        return (self._BUFFER[1] << 8) | self._BUFFER[0]
+
+    # pylint: enable=invalid-name
+
+    def _write_u8(self, address, val):
+        # Write an 8-bit unsigned value to the specified 8-bit address.
+        with self._device as i2c:
+            # Make sure to add command bit to write request.
+            self._BUFFER[0] = (address) & 0xFF
+            self._BUFFER[1] = val & 0xFF
+            i2c.write(self._BUFFER, end=2)
+            
+    def rfm69_data_avail(self):
+        rx_avail = 0
+        try:
+            rx_avail =self._read_u8(RFM69_RX_AVAIL)
+            # print('Rx Available = ',rx_avail)
+        except:
+        # print('Failed when bus.read_byte_data')
+        return rx_avail
+ 
+      
+    def read_rfm69_msg(self,rd_buff):
+        do_continue = False
+        rd_len = 0
+        if self.rfm69_data_avail(self) > 0:
+            try:            
+                    rd_len = self._read_u8(RFM69_RX_LOAD_MSG)
+                    print('rd_len=',rd_len)
+                except:
+                    do_continue = False
+                    print('LOAD_MSG Error')
+        else:
+            do_continue = False
+
+    
+    def enable(self):
+        """Put the device in a fully powered enabled mode."""
+        self._write_u8(0)
+
+    def disable(self):
+        """Disable the device and go into low power mode."""
+        self._write_u8(1)
